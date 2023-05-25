ec66cbee1cb73b5ade4d9889c8104d708e71e0c1
Danylo Pavlusenko <danil2005oleg@gmail.com>
2023-05-25 15:29:17 +0300

    New icon

--- obj/Debug/net6.0-windows/Pages/MainPageCategories/View/RecordsView.g.i.cs
+++ obj/Debug/net6.0-windows/Pages/MainPageCategories/View/RecordsView.g.i.cs
@@ -229,7 +229,7 @@ namespace SnakeGame.Pages.MainPageCategories.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SnakeGame;component/pages/mainpagecategories/view/recordsview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SnakeGame;V1.0.0.0;component/pages/mainpagecategories/view/recordsview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\..\Pages\MainPageCategories\View\RecordsView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
